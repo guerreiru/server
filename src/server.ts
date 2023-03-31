e6e85d01c8d3867e9598e588d857d9db924aaa9d
Fernando Guerreiro <fernando.rodrigues@prodap.com.br>
2023-03-31 16:37:04 -0300

    Commit inicial do server da aplicacao de chat

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import server from "http";
+import {Server} from "socket.io";
+
+const app = express();
+const serverHttp = server.createServer(app);
+const io = new Server(serverHttp, {
+  cors: {origin: "http://localhost:5173"}
+});
+
+io.on('connection', socket => {
+  console.log('Conectado', socket.id);
+
+  socket.on('disconnect', reason => {
+    console.log('Usuário desconectado', socket.id);
+  })
+
+  socket.on('set_username', username => {
+    socket.data.username = username;  
+  })
+
+  socket.on('message', text => {
+    io.emit('received_message', {
+      text,
+      authorId: socket.id,
+      author: socket.data.username || '',
+      messageId: new Date().getTime(),
+    });
+  })
+})
+
+serverHttp.listen(3333, () => {
+  console.log('Server listening on 3333');
+})
